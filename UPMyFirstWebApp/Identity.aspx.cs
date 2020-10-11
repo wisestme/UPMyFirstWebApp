879607a0440c96ebc59a4a5045f307c978b7ac09
wisestMe <wyhzest@gmail.com>
2020-10-11 15:17:20 +0100

    Add project files.

---
+++ UPMyFirstWebApp/Identity.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UPMyFirstWebApp
+{
+    public partial class Identity : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            string firstName = firstNameBox.Text;
+            string lastName = lastNameBox.Text;
+
+            string result = "Hello " + firstName + " " + lastName;
+
+            resultLabel.Text = result;
+        }
+    }
+}
